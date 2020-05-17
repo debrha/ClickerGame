06d1060a43b343568e88974417a96c836e7ffb52
debrenee <deb_rha@yahoo.com>
2020-05-17 11:02:36 -0500

    implemented more of the game

--- Assets/Scripts/DisasterScript.cs
+++ Assets/Scripts/DisasterScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class DisasterScript : MonoBehaviour
 {
     public GameObject statusBox;
-    public float cookieCheck;
+    private float cookieCheck;
     public int genChance;
     public bool disasterActive = false;
     public int cookieLoss;
@@ -14,7 +14,7 @@ public class DisasterScript : MonoBehaviour
 
     void Update()
     {
-        cookieCheck = GlobalCookies.CookieCount / 10;
+        cookieCheck = GlobalCookies.CookieCount / 5;
         if (disasterActive == false)
         {
             StartCoroutine(StartDisaster());
@@ -24,8 +24,12 @@ public class DisasterScript : MonoBehaviour
     {
         disasterActive = true;
         genChance = Random.Range(1, 20);
+        Debug.Log("Checking for Disaster " + cookieCheck + " vs " + genChance);
         if (cookieCheck >= genChance)
         {
+            Debug.Log("Starting Disaster");
+            fireAnim.SetActive(true);
+
             cookieLoss = Mathf.RoundToInt(GlobalCookies.CookieCount * 0.25f);
             statusBox.GetComponent<Text>().text = "You lost " + cookieLoss + " cookies in a factory fire!";
             GlobalCookies.CookieCount -= cookieLoss;
@@ -33,7 +37,9 @@ public class DisasterScript : MonoBehaviour
             statusBox.GetComponent<Animation>().Play("StatusAnim");
             yield return new WaitForSeconds(1);
             statusBox.SetActive(false);
+            fireAnim.SetActive(false);
             statusBox.SetActive(true);
+            Debug.Log("Stopping Disaster");
 
         }
         yield return new WaitForSeconds(10);
